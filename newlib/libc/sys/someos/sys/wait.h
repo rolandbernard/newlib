4b0255efb5387846fe542dc134cdd5ae26af0e93
Roland Bernard <rolli.bernard@gmail.com>
2021-10-29 21:27:06 +0200

    A number of fixes and changes

--- newlib/libc/sys/someos/sys/wait.h
+++ newlib/libc/sys/someos/sys/wait.h
@@ -4,7 +4,7 @@
 #define WNOHANG 1
 #define WUNTRACED 2
 
-#define WIFEXITED(val) ((val & 0xff) != 0)
+#define WIFEXITED(val) (((val >> 8) & 0xff) == 0)
 #define WEXITSTATUS(val) (val & 0xff)
 #define WIFSIGNALED(val) (((val >> 8) & 0xff) != 0)
 #define WTERMSIG(val) ((val >> 8) & 0xff)
